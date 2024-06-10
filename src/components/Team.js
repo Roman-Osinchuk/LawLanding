eb36b0bc74d138afb82bd2f71bc3a806934f1006
Roman <163313262+Roman-Osinchuk@users.noreply.github.com>
2024-06-10 22:02:09 +0300

    add interface for smartphone

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Team.css';
 import johnDoeImg from './img/bob.jpeg'; 
 import janeSmithImg from './img/ira.jpeg'; 
 import bobJohnsonImg from './img/stev.jpeg'; 
 
+const teamMembers = [
+  { img: johnDoeImg, name: 'Тарас Паляниця', experience: 'стаж 15 років.' },
+  { img: janeSmithImg, name: 'Оксана Гичко', experience: 'стаж 7 років.' },
+  { img: bobJohnsonImg, name: 'Микола Грибенко', experience: 'стаж 11 років.' }
+];
+
 const Team = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((prevIndex) => (prevIndex + 1) % teamMembers.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section id="team" className="section3">
       <div className="team-text">
-        <h2>Наші спеціалісти знають свою справу </h2>
-       <div className="team-fon"> <p>Я забезпечу подання Ваших інтересів у судах загальної юрисдикції, арбітражі, третейському суді, візьму участь у переговорах.</p></div>
-        <button class="team-button" onClick={() => window.scrollTo(0, 0)}>Зв'язатись з нами</button>
+        <h2>Наші спеціалісти знають свою справу</h2>
+        <div className="team-fon">
+          <p>Я забезпечу подання Ваших інтересів у судах загальної юрисдикції, арбітражі, третейському суді, візьму участь у переговорах.</p>
+        </div>
+        <button className="team-button" onClick={() => window.scrollTo(0, 0)}>Зв'язатись з нами</button>
       </div>
       <div className="team-members">
-        <div className="team-member">
-          <img src={johnDoeImg} alt="John Doe" />
-          <p className="member-name"><h4>Тарас Паляниця</h4>стаж 15 років.</p>
-        </div>
-        <div className="team-member">
-          <p className="member-name top"><h4>Оксана Гичко</h4>стаж 7 років.</p>
-          <img src={janeSmithImg} alt="Jane Smith" />
-        </div>
-        <div className="team-member">
-          <img src={bobJohnsonImg} alt="Bob Johnson" />
-          <p className="member-name"><h4>Микола Грибенко</h4>стаж 11 років.</p>
-        </div>
+        {teamMembers.map((member, index) => (
+          <div key={index} className={`team-member ${index === activeIndex ? 'active' : ''}`}>
+            <img src={member.img} alt={member.name} />
+            <p className="member-name"><h4>{member.name}</h4>{member.experience}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
